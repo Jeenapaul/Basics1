e6156a0249f702d50fe9fa75fefe4b4b37a4156f
jeenapaul <jeenapaul1014@gmail.com>
2022-06-30 11:11:18 -0400

    Initial commit

---
+++ src/com/JavaBasics.java
@@ -0,0 +1,31 @@
+package com;
+
+import java.util.Date;
+
+public class First {
+
+    public static void main(String args[]){
+        System.out.println("good");
+        byte b = -126;
+        short s = -32768;
+        int num = 100;
+        char c = 'A';
+        float f = 34.5f;
+        double d = 2344.67;
+        long l = 123333l;
+        String name  = "Jeena";
+        Boolean isOdd = true;
+        Date date = new Date();
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(num);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(name);
+        System.out.println(isOdd);
+        
+    }
+
+
+
+}
